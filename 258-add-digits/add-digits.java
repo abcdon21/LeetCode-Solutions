7ca66e24addc4cac232910ff5cdc96a5aaaf9adf
abhishi gupta <abhishigupta21@gmail.com>
2026-06-20 14:15:24 +0530

    Time: 1 ms (98.13%) | Memory: 42.6 MB (62.29%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int addDigits(int num) {
+        // int sum=0;
+        // if(num==0) return 0;
+        // else {
+        //  sum=  1+ ((num-1)%9);
+        // }
+        // return sum;
+
+        // return(n==0)
+
+         return (num==0?0:1+ ((num-1)%9));
+
+
+
+
+
+
+
+
+       
+        }
+    }
