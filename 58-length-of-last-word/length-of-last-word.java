034473e6f6c33ab635e7da61f10157b20dd4e55f
abhishi gupta <abhishigupta21@gmail.com>
2026-06-24 20:23:06 +0530

    Time: 1 ms (32.44%) | Memory: 43.3 MB (17.35%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        String words[]=s.trim().split(" ");
+        String last= words[words.length-1];
+        return last.length();
+        
+    }
+}
