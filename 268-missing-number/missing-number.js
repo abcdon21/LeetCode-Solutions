e8ca3510953ced195ca3afefcc7e6b50b24741cc
abhishi gupta <abhishigupta21@gmail.com>
2026-07-04 20:30:38 +0530

    Time: 2 ms (43.99%) | Memory: 56.3 MB (60.44%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    
+   
+    let sum2=0;
+    for(let i=0;i<nums.length;i++){
+        sum2 +=nums[i];
+    }
+
+    return nums.length*(nums.length+1)/2-sum2
+
+    
+};
