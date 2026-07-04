287a64bd8a9a1611b088f889822b1e663397edab
abhishi gupta <abhishigupta21@gmail.com>
2026-07-04 20:22:13 +0530

    Time: 44 ms (75.54%) | Memory: 53.8 MB (39.70%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n<2) return n;
+    let prev=0,curr=1,next;
+    for(let i=2;i<=n;i++){
+        next=prev+curr;
+        prev=curr;
+        curr=next;
+
+    }
+    return next;
+    
+   
+    
+};
