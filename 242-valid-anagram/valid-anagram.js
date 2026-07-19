99a336e153858137dd5eb88cc961c5e371975a2b
abhishi gupta <abhishigupta21@gmail.com>
2026-07-19 23:11:13 +0530

    Time: 15 ms (52.27%) | Memory: 61.4 MB (5.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,8 +4,16 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    //sort the string by converting it into array  .split("") and .sort()
-    //and then againg joining by the array by using .join()
-    return s.split("").sort().join()===t.split("").sort().join()
+    if(s.length!==t.length) return false;
+    let map ={};
+    for(let ch of s){
+        map[ch]=(map[ch]||0)+1
+    }
+    for(let ch of t){
+        if(!map[ch])
+            return false
+     map[ch]--;       
+    }
+    return true;
     
 };
