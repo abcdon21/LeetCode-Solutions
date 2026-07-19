36e344bafeae76d60b85988911a05368cd04c4d7
abhishi gupta <abhishigupta21@gmail.com>
2026-07-19 22:51:46 +0530

    Time: 2 ms (83.69%) | Memory: 55.3 MB (62.18%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+  var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    };
+
+    for (let ch of s) {
+        if (ch === '(' || ch === '{' || ch === '[')
+            stack.push(ch);
+        else {
+            if (stack.pop() !== map[ch])
+                return false;
+        }
+    }
+
+    return stack.length === 0;
+};
